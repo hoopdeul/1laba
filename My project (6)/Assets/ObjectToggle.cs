0aed8d92fe003a9bb6bdaeb567cf5d80fa9d3edb
hoopdeul <146860676+hoopdeul@users.noreply.github.com>
2023-10-03 19:11:56 +0300

    laba

---
+++ My project (6)/Assets/ObjectToggle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectToggle : MonoBehaviour
+{
+    public GameObject targetObject;
+    public Button toggleButton;
+    private bool isActive;
+
+    private void Start()
+    {
+        // Проверяем и устанавливаем начальное состояние объекта
+        isActive = targetObject.activeSelf;
+        UpdateButton();
+    }
+
+    public void ToggleObject()
+    {
+        isActive = !isActive;
+        targetObject.SetActive(isActive);
+        UpdateButton();
+    }
+
+    private void UpdateButton()
+    {
+        if (toggleButton != null)
+        {
+            toggleButton.GetComponentInChildren<Text>().text = isActive ? "Deactivate" : "Activate";
+        }
+    }
+}
